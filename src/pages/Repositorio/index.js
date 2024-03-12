1d8e3f84010500f6ad80920d9d2878edb034e434
Vinicius Troyack <troyackvinicius@gmail.com>
2024-03-11 22:24:22 -0300

    Criando Paginacao

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,7 +1,7 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import api from '../../services/api'
-import { Container, Owner, Loading, BackButton } from "./styles";
+import { Container, Owner, Loading, BackButton, IssuesList } from "./styles";
 import {FaArrowLeft} from 'react-icons/fa'
 
 export default function Repositorio() {
@@ -32,6 +32,7 @@ export default function Repositorio() {
         load();
 
     }, [repositorio])
+
     if(loading){
         return(
             <Loading>
@@ -54,6 +55,25 @@ export default function Repositorio() {
                 <h1>{repositorioInfo.name}</h1>
                 <p>{repositorioInfo.description}</p>
             </Owner>
+
+            <IssuesList>
+                {issues.map(issue => (
+                    <li key={String(issue.id)}>
+                        {console.log(issue)}
+                        <img src={issue.user.avatar_url} alt={issue.user.login}/> 
+                        <div>
+                            <strong>
+                                <a href={issue.html_url}>{issue.title}</a><br/>
+                                <p>Labels:</p>
+                                {issue.labels.map(label => (
+                                    <span key={String(label.id)}>{label.name}</span>
+                                ))}
+                            </strong>
+                            <p>{issue.user.login}</p>
+                        </div>
+                    </li>
+                ))}
+            </IssuesList>
         </Container>
     )
 }
